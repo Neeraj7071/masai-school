4f21d45822c3dd26d9c079fe9284d9c9859b5a93
Neeraj Khajuriya <97509661+Neeraj7071@users.noreply.github.com>
2022-03-24 00:03:49 +0530

    Delete ASSIGNMENT/src directory

--- ASSIGNMENT/src/configs/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = () => {
-    return mongoose.connect(
-      "mongodb+srv://neeraj:khajuriya1234@cluster0.vo7j0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    );
-  };
-  
