358cc5649d992c31f085626b6e893847497a1e61
Neeraj Khajuriya <97509661+Neeraj7071@users.noreply.github.com>
2022-03-28 11:30:00 +0530

    Update index.js

--- evalution/todosystem/src/index.js
+++ evalution/todosystem/src/index.js
@@ -1,7 +1,9 @@
 const express=require("express");
 const user=require("./controllers/auth.controller")
 const app=express();
+
 const todo=require("./controllers/todo.controller")
+app.use(express.json());
 app.use("/user",user)
 app.use("/todo",todo)
 module.exports=app
